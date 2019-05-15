e240638f75eac66c16b65cd94d69732be53a4649
MonoLogueChi <xxwhite.com@gmail.com>
2019-05-15 17:39:40 +0800

    feat: create new solution

---
+++ m-tools/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace m_tools.Models
+{
+	public class ErrorViewModel
+	{
+		public string RequestId { get; set; }
+
+		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+	}
+}
